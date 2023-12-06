409cc6f8811dab2d46ce7722f761d2862098b8a5
kh1012 <kh1012@midasit.com>
2023-12-07 07:46:57 +0900

    주석 업데이트

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import MainSample from './SampleComponents/MainSample'
 
+/**
+ * This is a sample code.
+ * for more information, please visit https://midasit-dev.github.io/moaui
+ */
 const App = () => {
 	return (
 		//You can modify the code here and test.
