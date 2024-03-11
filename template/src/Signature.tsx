30eb01e6f8eb15d854a004cb882a84ce0c4aa2cc
kh1012 <kh1012@midasit.com>
2024-03-11 18:35:04 +0900

    update 1.1.77

--- template/src/Signature.tsx
+++ template/src/Signature.tsx
@@ -10,7 +10,7 @@
  * @description sign of cra-template-moaui
  */
 
-const currentVersionFromPackageJson = '1.1.76';
+const currentVersionFromPackageJson = '1.1.77';
 
 export const log = () => {
 	/**
