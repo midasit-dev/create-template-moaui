01a31602bc56f600d40f7e43abe05afde167a71f
kh1012 <kh1012@midasit.com>
2024-04-22 18:05:07 +0900

    deploy npmjs (1.1.87)

--- template/src/Signature.tsx
+++ template/src/Signature.tsx
@@ -10,7 +10,7 @@
  * @description sign of cra-template-moaui
  */
 
-const currentVersionFromPackageJson = '1.1.83';
+const currentVersionFromPackageJson = '1.1.87';
 
 export const log = () => {
 	/**
